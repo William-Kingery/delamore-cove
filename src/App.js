83245cb83d894d5e84ab3eabfded2d5320b68f93
William-Kingery <william.kingery@gmail.com>
2024-07-09 09:51:11 -0400

    all packages added and any compiler errors

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home/Home";
+import Suites from "./pages/Suites/Suites";
+import Experiences from './pages/Experiences/Exp'; 
+import GettingHere from "./pages/GettingHere/GettingHere";
+import './App.scss';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+   return (
+      <BrowserRouter>
+         <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/suites" element={<Suites />} />
+            <Route path="/experiences" element={<Experiences />} />
+            <Route path="/getting-here" element={<GettingHere />} />
+         </Routes>
+      </BrowserRouter>
   );
 }
 
 export default App;
+
+
