3b8812569689a95b2f37fd14fde83cc598ec2e8f
William-Kingery <william.kingery@gmail.com>
2024-07-17 14:20:33 -0400

    photos added to assets & data, styling added to dropdown

--- src/data/suiteData.js
+++ src/data/suiteData.js
@@ -50,12 +50,14 @@ import kowhai15 from '../assets/images/kowhai/15.jpeg';
 import kowhai16 from '../assets/images/kowhai/16.jpeg';
 import kowhai17 from '../assets/images/kowhai/17.jpeg';
 import kowhai18 from '../assets/images/kowhai/18.jpeg';
+// ALL
+import layout from '../assets/images/general/1-layout.jpg'
 
 
 const suiteData = [
   {
     name: 'Kauri',
-    images: [ kauri5, kauri1, kauri2, kauri3, kauri4, kauri6, kauri7, kauri8, kauri9, kauri10, kauri11, kauri12, kauri13, kauri14],
+    images: [ kauri5, kauri1, kauri2, kauri3, kauri4, kauri6, kauri7, kauri8, kauri9, kauri10, kauri11, kauri12, kauri13, kauri14, layout],
     description: 'Kauri Suite offers breathtaking, uninterrupted views of the sea and bushland, with stunning sunsets stretching across the Bay to Motuihe Island and beyond. The interior boasts muted furnishings and thoughtful touches, creating a serene and comfortable space. High ceilings and expansive sliding doors lead to your own private balcony, enhancing the sense of openness and tranquility.',
     amenities: [
       'Queen Bed',
@@ -73,7 +75,7 @@ const suiteData = [
   },
   {
     name: 'Manuka',
-    images: [manuka9, manuka2, manuka3, manuka4, manuka5, manuka6, manuka7, manuka8],
+    images: [manuka9, manuka2, manuka3, manuka4, manuka5, manuka6, manuka7, manuka8, layout],
     description: 'Manuka Suite offers breathtaking, uninterrupted views of the sea and bushland, with stunning sunsets that span the Bay to Motuihe Island and beyond. The interior features sophisticated furnishings and thoughtful touches, creating a serene and comfortable atmosphere. High ceilings and large sliding doors open onto your private balcony, enhancing the sense of space and calm.',
     amenities: [
       'Queen Bed',
@@ -91,7 +93,7 @@ const suiteData = [
   },
   {
     name: 'Karaka',
-    images: [karaka3, karaka1, karaka2, karaka4, karaka5, karaka6, karaka7, karaka8],
+    images: [karaka3, karaka1, karaka2, karaka4, karaka5, karaka6, karaka7, karaka8, layout],
     description: "The Karaka Studio embodies the essence of New Zealand's local flora and natural beauty. The spacious, light-filled interior features high ceilings, a comfortable queen-size bed with a walk-in wardrobe, and an ensuite with heated flooring, all contributing to a luxurious feel. While the suite does not have a balcony, it offers a beautiful area with outdoor furniture, providing serene views of the garden—perfect for enjoying your morning coffee or an afternoon glass of wine. Thoughtful touches throughout the studio create a space with a true sense of style and comfort.",
     // 'The Karaka Studio embodies the essence of New Zealand\'s local flora and natural beauty. The spacious, light-filled interior features high ceilings, a comfortable queen-size bed with a walk-in wardrobe, and an ensuite with heated flooring, all contributing to a luxurious feel. Enjoy your morning coffee or afternoon glass of wine on the outdoor furniture, with serene views of the garden. Thoughtful touches throughout the studio create a space with a true sense of style and comfort.',//
     amenities: [
@@ -109,7 +111,7 @@ const suiteData = [
   },
   {
     name: 'Kowhai',
-    images: [kowhai2, kowhai1, kowhai3, kowhai4, kowhai5, kowhai6, kowhai7, kowhai8, kowhai9, kowhai10, kowhai11, kowhai12, kowhai13, kowhai14, kowhai15, kowhai17, kowhai18, kowhai16 ],
+    images: [kowhai2, kowhai1, kowhai3, kowhai4, kowhai5, kowhai6, kowhai7, kowhai8, kowhai9, kowhai10, kowhai11, kowhai12, kowhai13, kowhai14, kowhai15, kowhai17, kowhai18, kowhai16, layout],
     description: 'Ideal for families, Kowhai features a private covered porch and a BBQ area, perfect for outdoor gatherings. The well-equipped kitchenette has everything you need for a cozy evening in or an extended stay. The master bedroom offers a comfortable queen bed, while the second bedroom includes a bunk bed with a double bed on the bottom and a single on top. Please note: Access to the bathroom and porch is through the second bedroom.',
     amenities: [
       'Two Bedrooms: 1 Queen Bed, Double Bed & Single Bunk Bed',
