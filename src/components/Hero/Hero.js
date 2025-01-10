2af3b5740cb4c92938c65e546183cdedeae48249
William-Kingery <william.kingery@gmail.com>
2025-01-10 16:54:50 -0500

    made Hero component

---
+++ src/components/Hero/Hero.js
@@ -0,0 +1,74 @@
+import React from "react";
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import { ChevronLeft, ChevronRight } from 'lucide-react';
+
+const Hero = () => {
+    const images = [
+        "https://delamore-cove.s3.us-east-2.amazonaws.com/1-layout.png",
+        "https://delamore-cove.s3.us-east-2.amazonaws.com/10.jpg",
+        "https://delamore-cove.s3.us-east-2.amazonaws.com/11.jpg",
+        "https://delamore-cove.s3.us-east-2.amazonaws.com/12.jpg",
+        "https://delamore-cove.s3.us-east-2.amazonaws.com/13+11.35.19.jpg",
+        "https://delamore-cove.s3.us-east-2.amazonaws.com/4.jpg",
+        "https://delamore-cove.s3.us-east-2.amazonaws.com/2.jpg",
+        "https://delamore-cove.s3.us-east-2.amazonaws.com/3+11.35.19.jpg",
+        "https://delamore-cove.s3.us-east-2.amazonaws.com/5.jpg",
+        "https://delamore-cove.s3.us-east-2.amazonaws.com/6.jpg",
+        "https://delamore-cove.s3.us-east-2.amazonaws.com/7.jpg",
+        "https://delamore-cove.s3.us-east-2.amazonaws.com/8.jpg",
+        "https://delamore-cove.s3.us-east-2.amazonaws.com/14.jpg",
+        "https://delamore-cove.s3.us-east-2.amazonaws.com/15.jpg",
+        "https://delamore-cove.s3.us-east-2.amazonaws.com/19.jpg",
+    ];
+    
+     
+    const CustomPrevArrow = (props) => {
+    const { onClick } = props;
+    return (
+        <button className="hero__nav-button hero__nav-button--prev" onClick={onClick}>
+        <ChevronLeft size={24} />
+        </button>
+    );
+    };
+    
+    const CustomNextArrow = (props) => {
+    const { onClick } = props;
+    return (
+        <button className="hero__nav-button hero__nav-button--next" onClick={onClick}>
+            <ChevronRight size={24} />
+        </button>
+    );
+    };
+      
+    const settings = {
+        dots: false,
+        infinite: true,
+        speed: 1100,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 4000,
+        arrows: true,
+        prevArrow: < CustomPrevArrow />,
+        nextArrow: < CustomNextArrow />,
+    };
+    
+    
+    return (
+        <article>
+            <div className="hero">
+            <Slider {...settings}>
+               {images.map((image, index) => (
+                  <div className="hero__image-cont" key={index}>
+                  <img src={image} alt={`Slide ${index}`} className="hero__image" />
+                  </div>
+               ))}
+            </Slider>
+            </div>
+         </article>
+    );
+}
+
+export default Hero;
--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,66 +1,13 @@
 import React, { useEffect, useRef} from "react";
 import { Link, useNavigate} from "react-router-dom";
-import Slider from "react-slick";
-import "slick-carousel/slick/slick.css";
-import "slick-carousel/slick/slick-theme.css";
-import { ChevronLeft, ChevronRight } from 'lucide-react';
 import suiteData from '../../data/suiteData';
+import Hero from "../../components/Hero/Hero"
 import Header from "../../components/Header/Header";
 import Footer from "../../components/Footer/Footer";
 import "./Home.scss"
 
 
 const Home = () => {
-
-   const images = [
-      "https://delamore-cove.s3.us-east-2.amazonaws.com/1-layout.png",
-      "https://delamore-cove.s3.us-east-2.amazonaws.com/10.jpg",
-      "https://delamore-cove.s3.us-east-2.amazonaws.com/11.jpg",
-      "https://delamore-cove.s3.us-east-2.amazonaws.com/12.jpg",
-      "https://delamore-cove.s3.us-east-2.amazonaws.com/13+11.35.19.jpg",
-      "https://delamore-cove.s3.us-east-2.amazonaws.com/4.jpg",
-      "https://delamore-cove.s3.us-east-2.amazonaws.com/2.jpg",
-      "https://delamore-cove.s3.us-east-2.amazonaws.com/3+11.35.19.jpg",
-      "https://delamore-cove.s3.us-east-2.amazonaws.com/5.jpg",
-      "https://delamore-cove.s3.us-east-2.amazonaws.com/6.jpg",
-      "https://delamore-cove.s3.us-east-2.amazonaws.com/7.jpg",
-      "https://delamore-cove.s3.us-east-2.amazonaws.com/8.jpg",
-      "https://delamore-cove.s3.us-east-2.amazonaws.com/14.jpg",
-      "https://delamore-cove.s3.us-east-2.amazonaws.com/15.jpg",
-      "https://delamore-cove.s3.us-east-2.amazonaws.com/19.jpg",
-   ];
-
-   
-   const CustomPrevArrow = (props) => {
-      const { onClick } = props;
-      return (
-        <button className="hero__nav-button hero__nav-button--prev" onClick={onClick}>
-          <ChevronLeft size={24} />
-        </button>
-      );
-    };
-  
-   const CustomNextArrow = (props) => {
-      const { onClick } = props;
-      return (
-         <button className="hero__nav-button hero__nav-button--next" onClick={onClick}>
-            <ChevronRight size={24} />
-         </button>
-      );
-   };
-    
-   const settings = {
-      dots: false,
-      infinite: true,
-      speed: 1100,
-      slidesToShow: 1,
-      slidesToScroll: 1,
-      autoplay: true,
-      autoplaySpeed: 4000,
-      arrows: true,
-      prevArrow: < CustomPrevArrow />,
-      nextArrow: < CustomNextArrow />,
-   };
 
    const paragraphRefs = useRef([]);
    useEffect(() => {
@@ -104,17 +51,7 @@ const Home = () => {
    return (
       <main>
          <Header />
-         <article>
-            <div className="hero">
-            <Slider {...settings}>
-               {images.map((image, index) => (
-                  <div className="hero__image-cont" key={index}>
-                  <img src={image} alt={`Slide ${index}`} className="hero__image" />
-                  </div>
-               ))}
-            </Slider>
-            </div>
-         </article>
+         <Hero />
          <article className="article">
             <h1 className="article__header">Welcome to Delamore Cove</h1>
             <h2 className="article__subheader">WAIHEKE ISLAND, NEW ZEALAND</h2>
